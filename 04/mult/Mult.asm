9cde113255f42adc42941dac74a95aea03cba4bc
cmisenas <acolyte.camille@gmail.com>
2013-07-31 22:23:17 -0400

    Passed Project 04-mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+// r2 = r0 * r1
+// var r2 = r1;
+// var i = 1;
+// while(i < r0){
+//   r2 += r1;
+//   i++;
+// }
+// @ -> stored A register
+// M -> memory in A or RAM[A]
+// D -> just another register
+    @0
+    D=A
+    @2
+    M=D
+    @i
+    M=1
+(LOOP)
+    @1
+    D=M
+    @i
+    D=M-D
+    @END
+    D;JGT
+    @0
+    D=M
+    @2
+    M=M+D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
